2e3a18d6b6dd0f4afe8c9063cb8394c04351eb7d
Simon Bestler <68077359+bestler@users.noreply.github.com>
2022-11-16 09:30:17 +0100

    Initial Commit

---
+++ Quizz/QuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzApp.swift
+//  Quizz
+//
+//  Created by Simon Bestler on 16.11.22.
+//
+
+import SwiftUI
+
+@main
+struct QuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
