28b522deef06e661b649adb5b7b7fb3393663891
Simon Bestler <68077359+bestler@users.noreply.github.com>
2022-11-22 16:22:25 +0100

    Refactoring QuizView

--- Quizz/Views/QuizView.swift
+++ Quizz/Views/QuizView.swift
@@ -14,46 +14,27 @@ struct QuizView: View {
     
     @EnvironmentObject var questionRepository : QuestionRepository
     @StateObject var quizVM : QuizVM
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         VStack {
-            HStack() {
-                VStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color(.lightGray))
-                    Text("Simon").font(.footnote)
-                    HStack{
-                        ForEach(0..<3){i in
-                            if let point = quizVM.pointMask[i] {
-                                Circle().foregroundColor(point ? .green : .red)
-                                
-                            }else {
-                                Circle().foregroundColor(Color(.lightGray))
-                            }
-                        }
-                    }.frame(maxHeight: 20)
-                }
-                Spacer()
-            }
-            .frame(maxHeight: 100)
-            .padding(30)
+            Spacer()
             GroupBox(label:
-                        Label(quizVM.category.name.rawValue, systemImage: quizVM.category.iconName)
-                .foregroundColor(Color(quizVM.category.color))
-            ) {
+                        Label(quizVM.category.name.rawValue,
+                              systemImage: quizVM.category.iconName).accessibilityHidden(true)) {
                 Text(quizVM.currentQuestion!.question)
                     .padding(.vertical)
             }
-            .padding()
+                              .padding()
             if !quizVM.isShowNextQuestion {
                 ProgressView(timerInterval: quizVM.startDate...quizVM.endDate).self
                     .padding()
             }
             answerArea
                 .padding()
-                .frame(maxHeight: 300)
+                .frame(idealHeight: 500, maxHeight: 500)
+            PointView(pointMask: quizVM.pointMask)
+                .padding()
             if quizVM.isShowNextQuestion {
                 withAnimation{
                     Button("Next Question") {
@@ -75,52 +56,57 @@ struct QuizView: View {
             questionRepository.removeUsedQuestions(category: quizVM.category)
         }
         .navigationBarBackButtonHidden(true)
-        //.background(Color(quizVM.category.color))
     }
     
+
+    
     var answerArea : some View {
-        
         Grid(){
-            GridRow {
-                ForEach(0..<2){ i in
-                    let answer = quizVM.currentAnswers[i]
-                    ZStack {
-                        QuizCardBackground(isCorrect: answer.1,
-                                           isMasked: quizVM.mask[i])
-                        Text(quizVM.currentAnswers[i].0)
-                    }
-                    .onTapGesture {
-                        withAnimation {
-                            quizVM.selectedAnswerPos = i
-                            quizVM.evaluteQuestion()
-                        }
-                    }
-                }
-            }
-            GridRow{
-                ForEach(2..<4){ i in
-                    let answer = quizVM.currentAnswers[i]
-                    ZStack {
-                        QuizCardBackground(isCorrect: answer.1,
-                                           isMasked: quizVM.mask[i])
-                        Text(quizVM.currentAnswers[i].0)
-                    }
-                    .onTapGesture {
-                        withAnimation() {
-                            quizVM.selectedAnswerPos = i
-                            quizVM.evaluteQuestion()
+            ForEach(0..<2) {col in
+                let offset = col*2
+                GridRow {
+                    ForEach(offset..<offset+2, id:\.self){ i in
+                        let answer = quizVM.currentAnswers[i]
+                        QuizCard(answer: answer, isCorrect: answer.1,
+                                 isMasked: quizVM.mask[i])
+                        .onTapGesture {
+                            withAnimation {
+                                quizVM.selectedAnswerPos = i
+                                quizVM.evaluteQuestion()
+                            }
                         }
                     }
                 }
             }
         }
+        
+    }
+    
+}
+
+struct PointView : View {
+    
+    let pointMask : [Bool?]
+    
+    var body: some View {
+        HStack{
+            ForEach(0..<3){i in
+                if let point = pointMask[i] {
+                    Circle().foregroundColor(point ? .green : .red)
+                    
+                }else {
+                    Circle().foregroundColor(Color(.lightGray))
+                }
+            }
+        }.frame(maxHeight: 20)
     }
     
 }
 
 
-struct QuizCardBackground : View {
+struct QuizCard : View {
     
+    let answer : (String, Bool)
     let isCorrect : Bool
     let isMasked : Bool
     
@@ -129,10 +115,12 @@ struct QuizCardBackground : View {
     }
     
     var body: some View {
-        withAnimation{
+        ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .foregroundColor(isMasked ? color : Color(.secondarySystemBackground))
+            Text(answer.0)
         }
+        
     }
     
     struct Constants {
@@ -140,21 +128,20 @@ struct QuizCardBackground : View {
         static let colorTrue : Color = .green
         static let colorWrong : Color = .red
     }
+}
+
+
+struct QuizView_Previews: PreviewProvider {
     
     
-    
-    
-    struct QuizView_Previews: PreviewProvider {
-        
-        
-        static let exampleQuestion = Question(category: Question.Category.Computer,
-                                              type: Question.QuestionType.multiple,
-                                              difficulty: Question.Difficulty.hard,
-                                              question: "Which of the following is the oldest of these computers by release date?",
-                                              correct_answer: "TRS-80",
-                                              incorrect_answers: ["Commodore 64","ZX Spectrum","Apple 3"])
-        static var previews: some View {
-            QuizView(quizVM: QuizVM(quiz: Quiz(category: QuestionRepository.categories[0], questions: [exampleQuestion]))).environmentObject(QuestionRepository())
-        }
+    static let exampleQuestion = Question(category: Question.Category.Computer,
+                                          type: Question.QuestionType.multiple,
+                                          difficulty: Question.Difficulty.hard,
+                                          question: "Which of the following is the oldest of these computers by release date?",
+                                          correct_answer: "TRS-80",
+                                          incorrect_answers: ["Commodore 64","ZX Spectrum","Apple 3"])
+    static var previews: some View {
+        QuizView(quizVM: QuizVM(quiz: Quiz(category: QuestionRepository.categories[2], questions: [exampleQuestion]))).environmentObject(QuestionRepository())
     }
 }
+
